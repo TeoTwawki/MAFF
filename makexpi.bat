1abae9c63fe4fc34ee44566f98f962905f0eb494
xknight <xknight@localhost>
2005-08-09 18:21:10 -0400

    Simple modifications to build the 0.6.3 release from CVS

--- makexpi.bat
+++ makexpi.bat
@@ -2,6 +2,11 @@ del /y src\components\cvs\*.*
 rmdir src\components\cvs
 del /y src\components\maf.0.4.1.xpt
 del /y src\components\maf.0.4.3.xpt
+del /y src\components\maf.0.5.0.xpt
+del /y src\components\maf.0.5.1.xpt
+del /y src\components\maf.0.6.0.xpt
+del /y src\components\maf.0.6.1.xpt
+del /y src\components\maf.0.6.2.xpt
 
 del /y src\maf\cvs\*.*
 rmdir src\maf\cvs
@@ -22,10 +27,12 @@ rmdir src\maf\locale\ru-RU\cvs
 
 del /y src\scripts\cvs\*.*
 rmdir src\scripts\cvs
+del /y src\libs\cvs\*.*
+rmdir src\libs\cvs
 
 mkdir build
 cd build
-del /y maf-0.5.0.xpi
+del /y maf-0.6.3.xpi
 mkdir common
 cd common
 mkdir chrome
@@ -33,7 +40,7 @@ cd ..\..\src\maf
 del /y ..\..\build\common\chrome\maf.jar
 ..\scripts\zip -r ..\..\build\common\chrome\maf.jar .
 cd ..\..\build\common
-..\..\src\scripts\zip -r ..\maf-0.5.0.xpi .
+..\..\src\scripts\zip -r ..\maf-0.6.3.xpi .
 cd ..\..\src
-scripts\zip -r ..\build\maf-0.5.0.xpi install.* maf.html components scripts
+scripts\zip -r ..\build\maf-0.6.3.xpi install.* maf.html components scripts
 cd ..
--- makexpi.sh
+++ makexpi.sh
@@ -2,6 +2,11 @@
 rm -frd src/components/CVS
 rm -f src/components/maf.0.4.1.xpt
 rm -f src/components/maf.0.4.3.xpt
+rm -f src/components/maf.0.5.0.xpt
+rm -f src/components/maf.0.5.1.xpt
+rm -f src/components/maf.0.6.0.xpt
+rm -f src/components/maf.0.6.1.xpt
+rm -f src/components/maf.0.6.2.xpt
 rm -frd src/maf/CVS
 rm -frd src/maf/content/CVS
 rm -frd src/maf/locale/CVS
@@ -11,10 +16,11 @@ rm -frd src/maf/locale/it-IT/CVS
 rm -frd src/maf/locale/pl-PL/CVS
 rm -frd src/maf/locale/ru-RU/CVS
 rm -frd src/scripts/CVS
+rm -frd src/libs/CVS
 
 mkdir build
 cd build
-rm -f maf-0.5.0.xpi
+rm -f maf-0.6.3.xpi
 mkdir common
 cd common
 mkdir chrome
@@ -22,7 +28,7 @@ cd ../../src/maf
 rm -f ../../build/common/chrome/maf.jar
 zip -r ../../build/common/chrome/maf.jar .
 cd ../../build/common
-zip -r ../maf-0.5.0.xpi .
+zip -r ../maf-0.6.3.xpi .
 cd ../../src
-zip -r ../build/maf-0.5.0.xpi install.* maf.html components scripts
+zip -r ../build/maf-0.6.3.xpi install.* maf.html components scripts
 cd ..
