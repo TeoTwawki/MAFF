9608d0d5cafa0a457f629101408338d9b621d8e3
Paolo Amadini <http://www.amadzone.org/@localhost>
2009-04-08 13:35:19 +0200

    Improve reporting of warnings and errors generated by the Save Complete worker object

--- src/chrome/content/savecomplete/saveCompletePersistObject.js
+++ src/chrome/content/savecomplete/saveCompletePersistObject.js
@@ -98,24 +98,32 @@ SaveCompletePersist.prototype = {
       // Find the path of the file to save to
       var fileObject = aFile.QueryInterface(Ci.nsIFileURL).file;
 
+      // Store the URL of the document being saved for reference
+      this._saveUrlSpec = aDocument.location.href;
+
       // Save the selected page to disk
       var thisObject = this;
       var scOptions = {
         rewriteLinks: true,
-        callback: function(aSaver, aStatus, aOtherInfo) {
-          // Report the error messages, if present
-          if (aOtherInfo.errors && aOtherInfo.errors.length) {
-            // ------------------------------------------------------------
-            //  Errors that are notified here are typically network issues
-            //  or problems with the page being saved, that are intercepted
-            //  and reported by the integrated Save Complete component.
-            // ------------------------------------------------------------
-            Cu.reportError(aOtherInfo.errors);
+        callback: function(aSaver, aStatus, aResultObject) {
+          try {
+            // If the operation was canceled, update the result object to
+            //  reflect this condition, since Save Complete only provides the
+            //  NS_ERROR_FAILURE result code
+            if (thisObject.result == Cr.NS_BINDING_ABORTED) {
+              aResultObject.result = Cr.NS_BINDING_ABORTED;
+            }
+            // Report errors to the user through the progress listener, and log
+            //  both errors and warnings to the error console
+            thisObject._reportSaveCompleteErrors(aResultObject);
+            // Update the state of this object. The progress listener will be
+            //  notified later by the worker object itself.
+            thisObject.result = aResultObject.result;
+            thisObject._onComplete();
+          } catch (e) {
+            // Ignore any error during the error reporting phase
+            Cu.reportError(e);
           }
-          // Update the state of this object. The progress listener will be
-          //  notified later by the worker object itself.
-          thisObject.result = aOtherInfo.result;
-          thisObject._onComplete();
         },
         progressListener: this.progressListener
       };
@@ -161,5 +169,94 @@ SaveCompletePersist.prototype = {
     }
   },
 
-  _saver: null
+  /**
+   * Reports Save Complete results to the user and to the Error Console.
+   *
+   * @param aResultObject.nsresult   Final result code for the operation. If
+   *                                  this code is NS_OK, usually no errors are
+   *                                  present, but there may be some warnings.
+   * @param aResultObject.warnings   Optional array of strings with the warnings
+   *                                  to be reported. Warnings are only reported
+   *                                  to the Error Console.
+   * @param aResultObject.errors     Optional array of strings with the errors
+   *                                  to be reported. If present, the progress
+   *                                  listener will be notified.
+   */
+  _reportSaveCompleteErrors: function(aResultObject) {
+    // Report error and warning messages, if any, to the Error Console
+    this._reportToConsole(aResultObject.errors, true);
+    this._reportToConsole(aResultObject.warnings, false);
+    // If the download failed for any reason except explicit canceling
+    if (this.progressListener && aResultObject.result != Cr.NS_OK &&
+     aResultObject.result != Cr.NS_BINDING_ABORTED) {
+      // Report the error condition to the progress listener
+      var messageText = this._formattedStr("savecomplete.status.errors.msg",
+       [this._saveUrlSpec]) + "\n\n" +
+       this._str("savecomplete.status.savesystemtip.msg");
+      this.progressListener.onStatusChange(null, null, aResultObject.result,
+       messageText);
+    }
+  },
+
+  /**
+   * Reports Save Complete errors or warnings to the Error Console.
+   *
+   * @param aMessageArray   Array of strings to be reported. All the strings
+   *                         will be reported as a single message. If not
+   *                         provided or empty, no message will be reported.
+   * @param aIsError        Specify true if the messages are fatal errors, or
+   *                         false if they should reported as warnings.
+   */
+  _reportToConsole: function(aMessageArray, aIsError) {
+    // Check if no messages are available
+    if (!aMessageArray || !aMessageArray.length) {
+      return;
+    }
+
+    // Build the text of the single message
+    var allMessages = Array.join(aMessageArray, "\n");
+    var stringId = (aIsError ? "savecomplete.console.errors.text" :
+     "savecomplete.console.warnings.text");
+    var text = this._formattedStr(stringId, [this._saveUrlSpec, allMessages]);
+
+    // Determine the error severity
+    var flags = (aIsError ? Ci.nsIScriptError.errorFlag :
+     Ci.nsIScriptError.warningFlag);
+
+    // Create a detailed script error object for the Error Console. For
+    //  essential nsIScriptError documentation, see
+    //  <http://www.xulplanet.com/references/xpcomref/ifaces/nsIScriptError.html>
+    //  (retrieved 2009-04-08). The error category is the same used in the
+    //  Mozilla source file "dom/base/nsJSEnvironment.cpp".
+    var scriptError = Cc["@mozilla.org/scripterror;1"].
+     createInstance(Ci.nsIScriptError);
+    scriptError.init(text, null, null, 0, 0, flags, "chrome javascript");
+
+    // Report the error or warning to the Error Console
+    Cc["@mozilla.org/consoleservice;1"].getService(Ci.nsIConsoleService).
+     logMessage(scriptError);
+  },
+
+  /**
+   * Returns the string whose key is specified from the object's string bundle.
+   */
+  _str: function(aKey) {
+    return this._saveCompletePersistStrBundle.GetStringFromName(aKey);
+  },
+
+  /**
+   * Returns the string whose key is specified from the object's string bundle,
+   *  populating it with the arguments in the given array.
+   */
+  _formattedStr: function(aKey, aArgs) {
+    return this._saveCompletePersistStrBundle.formatStringFromName(aKey,
+     aArgs, aArgs.length);
+  },
+
+  _saveCompletePersistStrBundle: Cc["@mozilla.org/intl/stringbundle;1"]
+    .getService(Ci.nsIStringBundleService).createBundle(
+    "chrome://maf/locale/saveCompletePersistObject.properties"),
+
+  _saver: null,
+  _saveUrlSpec: ""
 }
