6a63c266d92692d5266427f37bbc080a118b7546
Paolo Amadini <http://www.amadzone.org/@localhost>
2009-05-09 18:40:35 +0200

    Remove old code from the shared state object

--- src/chrome/content/jscomponents/nsMafState.js
+++ src/chrome/content/jscomponents/nsMafState.js
@@ -28,7 +28,6 @@ var MAFNamespace = "http://maf.mozdev.org/metadata/rdf#";
 
 var gRDFService = null;
 var gRDFCService = null;
-var gNETIOService = null;
 
 
 function GetMafStateServiceClass() {
@@ -40,11 +39,6 @@ function GetMafStateServiceClass() {
   if (gRDFCService == null) {
     gRDFCService = Components.classes["@mozilla.org/rdf/container-utils;1"]
                      .getService(Components.interfaces.nsIRDFContainerUtils);
-  }
-
-  if (gNETIOService == null) {
-    gNETIOService = Components.classes["@mozilla.org/network/io-service;1"]
-                      .getService(Components.interfaces.nsIIOService);
   }
 
   if (!sharedData.MafStateService) {
@@ -70,19 +64,10 @@ MafStateServiceClass.prototype = {
   /** A count of the number of open archives. */
   noOfArchives: 0,
 
-  /** A list of local file urls and the network url it maps to */
-  localFileToUrlMap: new Array(),
-
-  /** A list of network urls and the local file url it maps to */
-  urlToLocalFileMap: new Array(),
-
-  /** A list of local file urls and the maf url it maps to */
-  localFileToMafUrlMap: new Array(),
-
   /**
    * Add archive file info to the state.
    */
-  addArchiveInfo: function(archive, count, localUrls) {
+  addArchiveInfo: function(archive) {
     this.noOfArchives++;
 
     var archiveRootSubjectStr = "http://maf.mozdev.org/metadata/rdf/archive/" + this.noOfArchives + "/";
@@ -90,9 +75,8 @@ MafStateServiceClass.prototype = {
     archive1Subject = gRDFService.GetResource(archiveRootSubjectStr);
     archive1Sequence = gRDFCService.MakeSeq(this.datasource, archive1Subject);
 
-    localUrls.value = this._addArchivePagesToDatasource(archiveRootSubjectStr, archive1Sequence,
-                                                         MafUtils.getURIFromFilename(archive.file.path), archive);
-    count.value = localUrls.value.length;
+    this._addArchivePagesToDatasource(archiveRootSubjectStr, archive1Sequence,
+                                                         archive.uri, archive);
 
     var archiveRootSubject = gRDFService.GetResource(archiveRootSubjectStr);
     var archivePredicate = gRDFService.GetResource(MAFNamespace + "title");
@@ -116,45 +100,22 @@ MafStateServiceClass.prototype = {
    * For each saved page, there's an RDF file, process it to get the information.
    */
   _addArchivePagesToDatasource: function(archiveSubjectRoot, archiveSequence, archiveUri, archive) {
-    var localUrls = new Array();
     // For each folder in the expanded archive root
     archive.pages.forEach(function(page, pageIndex) {
-      if (archive instanceof MaffArchive) {
-        try {
-          page._loadMetadata();
-        } catch (e) {
-          Cu.reportError(e);
-        }
-      }
-
       var title = page.title || ("Page " + (pageIndex + 1));
       var originalurl = page.originalUrl || "Unknown";
       var archivetime = page.dateArchived || "Unknown";
-      var indexfilename = page.indexLeafName || "index.html";
-
-      var archiveFilePart = page.tempDir.leafName + "/" + indexfilename;
-
-      var indexhtmlfile = page.tempDir.clone();
-      indexhtmlfile.append(indexfilename);
+      var localurl = page.tempUri.spec;
 
       var thisPageRDFSubjectRoot = archiveSubjectRoot + "" + (pageIndex + 1) + "/";
-
-      // get file URL - temp + expandedArchiveRoot + currFolder + index.html
-      var localurl = MafUtils.getURI(indexhtmlfile.nsIFile);
-
-      localUrls[localUrls.length] = localurl;
-
-      this.addPageInfoToMetaData(thisPageRDFSubjectRoot, title, originalurl, archivetime, localurl, archiveSequence,
-                                  archiveUri, archiveFilePart);
+      this.addPageInfoToMetaData(thisPageRDFSubjectRoot, title, originalurl, archivetime, localurl, archiveSequence);
     }, this);
-    return localUrls;
   },
 
   /**
    * Asserts the RDF info.
    */
-  addPageInfoToMetaData: function(thisPageRDFSubjectRoot, title, originalurl, archivetime, localurl, archiveSequence,
-                                   archiveUri, archiveFilePart) {
+  addPageInfoToMetaData: function(thisPageRDFSubjectRoot, title, originalurl, archivetime, localurl, archiveSequence) {
     // Add the page info to the metadata
 
     // This archive's unique archive URL resource
@@ -181,90 +142,5 @@ MafStateServiceClass.prototype = {
     this.datasource.Assert(rootSubject, predicate, object, true);
 
     archiveSequence.AppendElement(rootSubject);
-
-    // Add the info to a searchable URL map.
-    this.localFileToUrlMap[localurl] = originalurl;
-    this.urlToLocalFileMap[originalurl] = localurl;
-
-    // Add maf protocol support
-    this.localFileToUrlMap["jar:" + archiveUri + "!/" + archiveFilePart] = originalurl
-    this.localFileToMafUrlMap[localurl] = "jar:" + archiveUri + "!/" + archiveFilePart;
-
-    // If the original url has a # sign, add the original url without the # sign to the list
-    if (originalurl.indexOf("#") > 0) {
-      this.urlToLocalFileMap[originalurl.substring(0,originalurl.indexOf("#"))] = localurl;
-    }
-  },
-
-  isArchiveURL: function(url) {
-    var result = false;
-
-    if (typeof(this.localFileToUrlMap[url]) != "undefined") {
-      result = true;
-    }
-
-    if (!result) {
-
-      // Could be a document in a frame
-      try {
-        // Try to get the file object's parent
-        var ouri = gNETIOService.newURI(url, "", null);    // Create URI object
-        var parent = ouri.QueryInterface(Components.interfaces.nsIFileURL).file.parent;
-
-        while ((parent != null) && (!result)) {
-          // If the parent index.htm or index.html is in the map, it's an archive url
-          var uri1 = MafUtils.getURIFromFilename(MafUtils.appendToDir(parent.path, "index.html"));
-          var uri2 = MafUtils.getURIFromFilename(MafUtils.appendToDir(parent.path, "index.htm"));
-          if ((typeof(this.localFileToUrlMap[uri1]) != "undefined") ||
-              (typeof(this.localFileToUrlMap[uri2]) != "undefined")) {
-            result = true;
-          }
-
-          parent = parent.parent;
-        }
-
-      } catch(e) {
-        // For URIs that are not files, exceptions are ignored.
-
-      }
-    }
-
-    return result;
-  },
-
-  isLocallyMappableURL: function(url) {
-    var result = false;
-
-    if (typeof(this.urlToLocalFileMap[url]) != "undefined") {
-      result = true;
-    }
-    return result;
-  },
-
-  getLocalURL: function(url) {
-    var result = url;
-
-    if (typeof(this.urlToLocalFileMap[url]) != "undefined") {
-      result = this.urlToLocalFileMap[url];
-    }
-    return result;
-  },
-
-  getOriginalURL: function(url) {
-    var result = url;
-
-    if (typeof(this.localFileToUrlMap[url]) != "undefined") {
-      result = this.localFileToUrlMap[url];
-    }
-    return result;
-  },
-
-  getMafURL: function(url) {
-    var result = url;
-
-    if (typeof(this.localFileToMafUrlMap[url]) != "undefined") {
-      result = this.localFileToMafUrlMap[url];
-    }
-    return result;
   }
 };
