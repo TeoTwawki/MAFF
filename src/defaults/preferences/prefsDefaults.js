25b59417c959b77a5ca9d4926d9a5774d57a6ffc
Paolo Amadini <http://www.amadzone.org/@localhost>
2011-06-27 11:54:47 +0200

    Add international data folder suffixes that can help in locating pages saved with browsers in other languages

--- src/defaults/preferences/prefsDefaults.js
+++ src/defaults/preferences/prefsDefaults.js
@@ -1,5 +1,5 @@
 pref("extensions.{7f57cf46-4467-4c2d-adfa-0cba7c507e54}.description", "chrome://maf/locale/addonDescription.properties");
-pref("extensions.maf.advanced.datafoldersuffixes", "_files;.files");
+pref("extensions.maf.advanced.datafoldersuffixes", "_files;.files;_amafayela;_amafayili;_archivos;_arquivos;_bestanden;_bestannen;_comhaid;-Dateien;_datoteke;_difaele;_dosieroj;_dosiye;_dosyalar;_dzifaela;_elemei;_emafayela;_faidhlichean;_failai;_faili;_failid;_fajlovi;_fayiro;_ficheiros;_ficheros;_fichiers;_fichièrs;-filer;_fitxategiak;_kartela;_lêers;_mfael;_pel;_pliki;_soubory;_subory;_tiedostot;_tukey;_подаци;_файлууд;_ֆայլեր;_फ़ाइल;_নথিপত্ৰ;_ਫਾਇਲਾਂ;_ફાઈલો;_கோப்புக்கள்");
 pref("extensions.maf.advanced.maff.compression", "dynamic");
 pref("extensions.maf.advanced.maff.extendedmetadata", false);
 pref("extensions.maf.advanced.maff.ignorecharacterset", false);
